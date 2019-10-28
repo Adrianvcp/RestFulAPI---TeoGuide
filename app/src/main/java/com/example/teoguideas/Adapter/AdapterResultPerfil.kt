912f5edee7550eafbf902c140d43da860304c131
Adrian Vela <u201416198@upc.edu.pe>
2019-10-27 22:46:23 -0600

    Interfaz camara funcionando pero sin guardar datos y boton simulador de camara que analiza la foto

--- app/src/main/java/com/example/teoguideas/Adapter/AdapterResultPerfil.kt
+++ app/src/main/java/com/example/teoguideas/Adapter/AdapterResultPerfil.kt
@@ -1,4 +1,4 @@
-package com.example.kotlincomicreader.Adapter
+package com.example.teoguideas.Adapter
 
 import android.content.Context
 import android.view.LayoutInflater
@@ -7,19 +7,20 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlincomicreader.Adapter.MyComicAdapter
 import com.example.teoguideas.Common.Common
 import com.example.teoguideas.Interface.IRecyclerOnClick
 import com.example.teoguideas.Model.CentroHistorico
 import com.example.teoguideas.R
 import com.squareup.picasso.Picasso
 
-class MyComicAdapter(internal var context: Context,
-                     internal var centroList:List<CentroHistorico>):
-RecyclerView.Adapter<MyComicAdapter.MyViewHolder>()
-{
+class AdapterResultPerfil(internal var context: Context,
+                          internal var centroList:List<CentroHistorico>):
+    RecyclerView.Adapter<AdapterResultPerfil.MyViewHolder>() {
+
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): MyViewHolder {
         val itemView = LayoutInflater.from(context)
-            .inflate(R.layout.centro_item,p0,false)
+            .inflate(R.layout.activity_perfil_recurso,p0,false)
         return MyViewHolder(itemView)
     }
 
@@ -30,17 +31,18 @@ RecyclerView.Adapter<MyComicAdapter.MyViewHolder>()
     override fun onBindViewHolder(p0: MyViewHolder, p1: Int) {
         Picasso.get().load(centroList[p1].imgportada).into(p0.centro_image)
         p0.centro_name.text = centroList[p1].nNombre
-        p0.setClick(object:IRecyclerOnClick{
+        p0.setClick(object: IRecyclerOnClick {
             override fun onClick(view: View, position: Int) {
                 Common.selected_centroH = centroList[position]
             }
         })
     }
 
-    inner class MyViewHolder(itemView:View): RecyclerView.ViewHolder(itemView),View.OnClickListener{
+    inner class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), View.OnClickListener{
 
-        internal var centro_image:ImageView
-        internal var centro_name:TextView
+        internal var centro_image: ImageView
+        internal var centro_name: TextView
+
         lateinit var iRecyclerOnClick: IRecyclerOnClick
 
         fun setClick(iRecyclerOnClick: IRecyclerOnClick){
@@ -49,7 +51,7 @@ RecyclerView.Adapter<MyComicAdapter.MyViewHolder>()
 
         init {
             centro_image = itemView.findViewById(R.id.centro_image) as ImageView
-            centro_name = itemView.findViewById(R.id.centro_name) as TextView
+            centro_name = itemView.findViewById(R.id.txtNombre) as TextView
         }
 
         override fun onClick(p0: View?) {
@@ -57,3 +59,4 @@ RecyclerView.Adapter<MyComicAdapter.MyViewHolder>()
         }
     }
 }
+
--- app/src/main/java/com/example/teoguideas/Retrofit/IComicAPI.kt
+++ app/src/main/java/com/example/teoguideas/Retrofit/IComicAPI.kt
@@ -6,8 +6,9 @@ import retrofit2.http.GET
 
 
 interface IComicAPI {
-
-
     @get:GET("centro")
     val CHistoList:Observable<List<CentroHistorico>>
+
+    @get:GET("buscarFoto")
+    val PerfilList:Observable<List<CentroHistorico>>
 }
