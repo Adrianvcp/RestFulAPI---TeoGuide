ac8d4dad6e2f3fd50531af240a2c42344121fc27
Andres Choque <andreschoquemx0@gmail.com>
2019-11-07 11:09:00 -0500

    scaffold fragments

---
+++ app/src/main/java/com/example/teoguideas/Controllers/Activities/RootActivity.kt
@@ -0,0 +1,13 @@
+package com.example.teoguideas.Controllers.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.teoguideas.R
+
+class RootActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_root)
+    }
+}
