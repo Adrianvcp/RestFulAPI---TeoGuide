1477ea816f45307a25f9f7790d61ef7a1b72f16f
Andres Choque <andreschoquemx0@gmail.com>
2019-11-07 19:39:23 -0500

    vista explorar  implementado

--- app/src/main/java/com/example/teoguideas/Controllers/Activities/RootActivity.kt
+++ app/src/main/java/com/example/teoguideas/Controllers/Activities/RootActivity.kt
@@ -2,12 +2,52 @@ package com.example.teoguideas.Controllers.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.example.teoguideas.Controllers.Fragments.ExplorarFragment
+import com.example.teoguideas.Controllers.Fragments.InicioFragment
+import com.example.teoguideas.Controllers.Fragments.PerfilFragment
+import com.example.teoguideas.Controllers.Fragments.PlanesFragment
 import com.example.teoguideas.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_root.*
+import kotlin.math.log
 
 class RootActivity : AppCompatActivity() {
 
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        navigateTo(item)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
+        val navView: BottomNavigationView = findViewById(R.id.navigationBar)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+       navigateTo(navView.menu.findItem(R.id.navigation_inicio))
+
+    }
+    private fun navigateTo(item: MenuItem): Boolean {
+        item.isChecked=true
+        return supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.content, getFragmentFor(item))
+            .commit() > 0
+    }
+    private fun getFragmentFor(item: MenuItem): Fragment {
+        if(item.itemId==R.id.navigation_inicio){
+            return InicioFragment()
+        }else if(item.itemId==R.id.navigation_explorar){
+            return ExplorarFragment()
+        }else if(item.itemId==R.id.navigation_planes){
+            return PlanesFragment()
+        }else if(item.itemId==R.id.navigation_perfil){
+            return PerfilFragment()
+        }
+        return InicioFragment()
     }
 }
--- app/src/main/java/com/example/teoguideas/Controllers/Fragments/ExplorarFragment.kt
+++ app/src/main/java/com/example/teoguideas/Controllers/Fragments/ExplorarFragment.kt
@@ -1,14 +1,34 @@
 package com.example.teoguideas.Controllers.Fragments
 
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
 
 import com.example.teoguideas.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_explorar.*
+import java.io.IOException
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,80 +43,107 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ExplorarFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ExplorarFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
+class ExplorarFragment : Fragment(), OnMarkerClickListener {
+    private lateinit var fusedLocationClient:FusedLocationProviderClient
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var lastLocation: Location
+    companion object{
+        private const val LOCATION_PERMISSION_REQUIREST_CODE=1
+    }
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        return false
     }
 
+    lateinit var mapFragment:SupportMapFragment
+    lateinit var googleMap: GoogleMap
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        fusedLocationClient=LocationServices.getFusedLocationProviderClient(requireActivity())
+
         return inflater.inflate(R.layout.fragment_explorar, container, false)
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        mapFragment=childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap=it
+            googleMap.setOnMarkerClickListener(this)
+            googleMap.uiSettings.isZoomControlsEnabled=true
+            setUpMap()
+            initSearchingMethods()
+        })
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+    override fun onResume() {
+        super.onResume()
+    }
+    private fun setUpMap(){
+        if(ActivityCompat.checkSelfPermission(requireContext(),android.Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+           requestPermissions(
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUIREST_CODE)
+            return
+        }
+        googleMap.isMyLocationEnabled = true
+        fusedLocationClient.lastLocation.addOnSuccessListener{
+            if(it!=null){
+
+                lastLocation=it
+                var currentLatLong=LatLng(it.latitude,it.longitude)
+                placeMarker(currentLatLong,"Mi ubicacion")
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom((currentLatLong),13f))
+            }
+
         }
     }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
+    private fun placeMarker(location:LatLng,title:String){
+        var marckerOption= MarkerOptions().position(location).title(title)
+        googleMap.addMarker(marckerOption)
     }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
+    private fun searchAction(){
+        geoLocate()
+        val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view?.windowToken, 0)
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ExplorarFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ExplorarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun initSearchingMethods(){
+        ic_magnify.setOnClickListener({
+            searchAction()
+        })
+        mSearchText.setOnEditorActionListener{v,actionId,event->
+            if(actionId== EditorInfo.IME_ACTION_DONE
+                || actionId==EditorInfo.IME_ACTION_DONE
+                || event.action==KeyEvent.ACTION_DOWN
+                || event.action==KeyEvent.KEYCODE_ENTER){
+                geoLocate()
+                val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(view?.windowToken, 0)
+                true
+            }else{
+                false
             }
+
+        }
+    }
+    fun geoLocate(){
+        var searchString=mSearchText.text.toString()
+        var geocoder:Geocoder= Geocoder(context)
+        var list:List<Address>? =null
+        try {
+            list=geocoder.getFromLocationName(searchString,1)
+        }catch (e:IOException){
+
+        }
+        var size:Int= list?.size?:0
+        if(size > 0){
+            var address:Address?=list?.get(0)
+            var coords=LatLng(address?.latitude?:0.0,address?.longitude?:0.0)
+            placeMarker(coords,address?.getAddressLine(0)?:"")
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom((coords),13f))
+
+        }
     }
 }
