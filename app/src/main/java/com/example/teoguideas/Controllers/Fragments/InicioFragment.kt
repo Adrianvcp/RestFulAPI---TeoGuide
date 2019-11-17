04ba1addde0fee4ff264a7355c03d542977f874a
Adrian Vela <u201416198@upc.edu.pe>
2019-11-16 21:24:57 -0500

    ok

--- app/src/main/java/com/example/teoguideas/Controllers/Fragments/InicioFragment.kt
+++ app/src/main/java/com/example/teoguideas/Controllers/Fragments/InicioFragment.kt
@@ -23,7 +23,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
--- app/src/main/java/com/example/teoguideas/Retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/teoguideas/Retrofit/RetrofitClient.kt
@@ -11,7 +11,7 @@ object RetrofitClient {
         get() {
             if (ourInstance == null)
                 ourInstance = Retrofit.Builder()
-                    .baseUrl("http://10.0.2.2:3000")
+                    .baseUrl("http://teoguide.herokuapp.com")
                     .addConverterFactory(GsonConverterFactory.create())
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .build()
